56fa4788141ed86c347e132b5ac6459f6e9bdaae
Hikawa Rin <releaseavalon@gmail.com>
2022-11-25 06:17:16 +0000

    Finish Lab 11

--- lab11/lab11.sql
+++ lab11/lab11.sql
@@ -2,16 +2,17 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet FROM students WHERE color = 'blue' and pet = 'dog';
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet, song FROM students WHERE color = 'blue' and pet = 'dog';
 
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, MIN(smallest) FROM students GROUP BY smallest Having COUNT(smallest) = 1;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color FROM students AS a, students AS b
+  WHERE a.time < b.time and a.pet = b.pet and a.song = b.song;
 
